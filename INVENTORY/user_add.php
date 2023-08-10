bd42a5c1caedd9a95f05a62d8c25689d60be915a
Hamna Iftikhar <96309521+hamnaiftikhar@users.noreply.github.com>
2023-08-10 11:40:41 +0500

    Update user_add.php
    how to delete and update user with bootstrap

--- INVENTORY/user_add.php
+++ INVENTORY/user_add.php
@@ -16,8 +16,9 @@
     <title>Dashboard - Inventory Management System</title>
     <link rel="stylesheet" type="text/css" href="css/login.css">
     <link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.35.4/css/bootstrap-dialog.min.css" integrity="sha512-PvZCtvQ6xGBLWHcXnyHD67NTP+a+bNrToMsIdX/NUqhw+npjLDhlMZ/PhSHZN4s9NdmuumcxKHQqbHlGVqc8ow==" crossorigin="anonymous" referrerpolicy="no-referrer" />
  <!--   <script src="https://kit.fontawesome.com/74f27641c2.js" crossorigin="anonymous"></script> -->
-
+    
 </head>
 <body>
     <div id="dashboardMainContainer">
@@ -65,8 +66,6 @@
                                     </p>
                                 </div>
                             <?php unset($_SESSION['response']); } ?>
-
-
                             </div>
                         </div>
                         <div class="column column-7">
@@ -82,17 +81,23 @@
                                                 <th>Email</th>
                                                 <th>Created At</th>
                                                 <th>Updated At</th>
+                                                <th>Action</th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             <?php foreach($users as $index => $user){ ?>
                                                 <tr>
                                                     <td><?= $index + 1 ?></td>
-                                                    <td><?= $user['first_name'] ?></td>
-                                                    <td><?= $user['last_name'] ?></td>
-                                                    <td><?= $user['email'] ?></td>
+                                                    <td class="firstName"><?= $user['first_name'] ?></td>
+                                                    <td class="lastName"><?= $user['last_name'] ?></td>
+                                                    <td class="email"><?= $user['email'] ?></td>
                                                     <td><?= date('M d,Y @ h:i:s A', strtotime($user['created_at'])) ?></td>
                                                     <td><?= date('M d,Y @ h:i:s A', strtotime($user['updated_at'])) ?></td>
+                                                    <td>
+                                                        <a href="" class="updateUser" data-userid="<?= $user['id'] ?>"><i class="fa fa-pencil" ></i> Edit </a>
+                                                        <a href="" class="deleteUser" data-userid="<?= $user['id'] ?>" data-fname="<?= $user['first_name'] ?>" data-lname="<?= $user['last_name'] ?>" ><i class="fa fa-trash"></i> Delete </a>
+                                                        
+                                                    </td>
                                                 </tr>
                                             <?php } ?>
                                         </tbody>
@@ -107,7 +112,162 @@
         </div>
     </div>
     
-<script src="Javascript/script.js">
+<script src="Javascript/script.js"></script>
+<script src="Javascript/JQuerry/jquery-3.7.0.js"></script>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!-- javascript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.35.4/js/bootstrap-dialog.js" integrity="sha512-AZ+KX5NScHcQKWBfRXlCtb+ckjKYLO1i10faHLPXtGacz34rhXU8KM4t77XXG/Oy9961AeLqB/5o0KTJfy2WiA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+    function script(){
+        
+        
+        this.initialize = function(){
+            this.registerEvents();
+        },
+            
+        this.registerEvents = function(){
+            document.addEventListener('click', function(e){
+                targetElement = e.target;
+                classList = targetElement.classList; 
+                
+                if(classList.contains('deleteUser')){
+                    e.preventDefault();
+                    userId = targetElement.dataset.userid;
+                    fname = targetElement.dataset.fname;
+                    lname = targetElement.dataset.lname;
+                    fullName = fname + ' ' + lname;
+                    
+                    BootstrapDialog.confirm({
+                        type: BootstrapDialog.TYPE_DANGER,
+                        message: 'Are you sure to delete '+ fullName +'?',
+                        callback: function(isDelete){
+                            $.ajax({
+                                method: 'POST',
+                                data: {
+                                    user_id: userId,
+                                    f_name: fname,
+                                    l_name: lname
+
+                                },
+                                url: 'database/delete-user.php',
+                                dataType : 'json',
+                                success: function(data){
+                                    if (data.success) {
+                                        BootstrapDialog.alert({
+                                            type: BootstrapDialog.TYPE_SUCCESS,
+                                            message: data.message,
+                                            callback: function() {
+                                                location.reload();
+                                            }
+                                        });
+                                    } else 
+                                        BootstrapDialog.alert({
+                                            type: BootstrapDialog.TYPE_DANGER,
+                                            message: data.message,
+                                        });
+                                }
+                            });
+                        }
+                    })
+                    
+                    /*if(window.confirm('Are you sure to delete '+ fullName +'?')){
+                        // jquerry plugin: ajax
+                        $.ajax({
+                            method: 'POST',
+                            data: {
+                                user_id: userId,
+                                f_name: fname,
+                                l_name: lname
+                                
+                            },
+                            url: 'database/delete-user.php',
+                            dataType : 'json',
+                            success: function(data){
+                                if(data.success){
+                                    if(window.confirm(data.message)){
+                                        location.reload();
+                                    }
+                                } else window.alert(data.message);
+                            }
+                        })
+                    } */
+                }
+                
+                if(classList.contains('updateUser')){
+                    e.preventDefault(); //prevent from loading
+                    
+                    // getting the data
+                    
+                    firstName = targetElement.closest('tr').querySelector('td.firstName').innerHTML;
+                    lastName = targetElement.closest('tr').querySelector('td.lastName').innerHTML;
+                    email = targetElement.closest('tr').querySelector('td.email').innerHTML;
+                    userId = targetElement.dataset.userid;
+                    
+                    BootstrapDialog.confirm({
+                        title: 'Update ' + firstName + ' ' + lastName,
+                        message: '<form>\
+                            <div class="form-group">\
+                                <label for="firstName">First Name:</label>\
+                                <input type="text" class="form-control" id="firstName" value="'+ firstName +'">\
+                            </div>\
+                            <div class="form-group">\
+                                <label for="lastName">Last Name:</label>\
+                                <input type="text" class="form-control" id="lastName" value="'+ lastName +'">\
+                            </div>\
+                            <div class="form-group">\
+                                <label for="email">Email address:</label>\
+                                <input type="email" class="form-control" id="emailUpdate" value="'+ email +'">\
+                            </div>\
+                        </form>',
+                        callback: function(isUpdate) {
+                            if(isUpdate) { // updates if user click updates button
+                                $.ajax({
+                                    method: 'POST',
+                                    data: {
+                                        userId: userId,
+                                        f_name: document.getElementById('firstName').value,
+                                        l_name: document.getElementById('lastName').value,
+                                        email: document.getElementById('emailUpdate').value,
+                                    },
+                                    url: 'database/update-user.php',
+                                    dataType : 'json',
+                                    success: function(data) {
+                                        if (data.success) {
+                                            BootstrapDialog.alert({
+                                                type: BootstrapDialog.TYPE_SUCCESS,
+                                                message: data.message,
+                                                callback: function() {
+                                                    location.reload();
+                                                }
+                                            });
+                                        } else {
+                                            BootstrapDialog.alert({
+                                                type: BootstrapDialog.TYPE_DANGER,
+                                                message: data.message,
+                                            });
+                                        }
+                                    }
+
+                                });
+                            }
+                        }
+                    });
+                    
+                }
+            });
+        }
+    }
+    
+    var script = new script;
+    script.initialize();
+    
+    
 </script>
 </body>
 </html>
