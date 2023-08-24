baa257eda9edaa74c1d5256e3c58e19f2e23958e
Hamna Iftikhar <96309521+hamnaiftikhar@users.noreply.github.com>
2023-08-24 22:50:49 +0500

    Update sidebar.php
    sidebar addtional features

--- INVENTORY/partials/sidebar.php
+++ INVENTORY/partials/sidebar.php
@@ -1,3 +1,10 @@
+<?php 
+    $user = $_SESSION['users'];
+
+?>
+
+
+
 <div class="dashboard_sidebar" id="dashboard_sidebar">
     <h3 class="dashboard_logo" id="dashboard_logo">Unilever</h3>
     <div class="dashboard_sidebar_user">
@@ -9,21 +16,68 @@
             
             <!-- class="menuActive" -->
             
-            <li>
+            <li class="liMainMenu">
                 <a href="./dashboard.php"><i class="fa fa-dashboard"></i><span class="menuText">Dashboard</span></a>
             </li>
-            <li>
-                <a href="./user_add.php"><i class="fa fa-user-plus"></i><span class="menuText">Add User</span></a>
+            <li class="liMainMenu"> 
+                <a href="javascript:void(0);" class="showHideSubMenu">  <!-- SUbmenus  LIST -->
+                <i class="fa fa-product-hunt showHideSubMenu"></i>
+                <span class="menuText showHideSubMenu">Product</span>
+                <i class="fa fa-angle-left mainMenuIconArrow showHideSubMenu"></i>
+                </a>
+                
+                <ul class="subMenus">
+                    <li><a class="subMenuLink" href="./product_view.php"><i class="fa fa-circle-o"></i>View Product</a></li>
+                    <li><a class="subMenuLink" href="./product_add.php"><i class="fa fa-circle-o"></i>Add Product</a></li>
+
+                </ul>
             </li>
-            <li>
-                <a href=""><i class="fa fa-money"></i><span class="menuText">REVENUE MANAGEMENT</span></a>
+            <li  class="liMainMenu" >
+                <a href="javascript:void(0);" class="showHideSubMenu">  <!-- SUbmenus  LIST -->
+                <i class="fa fa-industry showHideSubMenu"></i>
+                <span class="menuText showHideSubMenu">Supplier</span>
+                <i class="fa fa-angle-left mainMenuIconArrow showHideSubMenu"></i>
+                </a>
+                
+                <ul class="subMenus">
+                    <li><a class="subMenuLink" href="./supplier_view.php"><i class="fa fa-circle-o"></i>View Supplier</a></li>
+                    <li><a class="subMenuLink" href="./supplier_add.php"><i class="fa fa-circle-o"></i>Add Supplier</a></li>
+                </ul>
             </li>
-            <li>
-                <a href=""><i class="fa fa-cog"></i><span class="menuText">SETTINGS</span></a>
+            <li  class="liMainMenu" >
+                <a href="javascript:void(0);" class="showHideSubMenu">  <!-- SUbmenus  LIST -->
+                <i class="fa fa-first-order showHideSubMenu"></i>
+                <span class="menuText showHideSubMenu">Purchase Order</span>
+                <i class="fa fa-angle-left mainMenuIconArrow showHideSubMenu"></i>
+                </a>
+                
+                <ul class="subMenus">
+                    <li><a class="subMenuLink" href="./product_order.php"><i class="fa fa-circle-o"></i>Add New PO</a></li>
+                    <li><a class="subMenuLink" href="./view_order.php"><i class="fa fa-circle-o"></i>View Order</a></li>
+                </ul>
             </li>
-            <li>
+            
+            <li class="liMainMenu showHideSubMenu">
+                <a href="javascript:void(0);" class="showHideSubMenu">  <!-- SUbmenus  LIST -->
+                    <i class="fa fa-user-plus showHideSubMenu"></i>
+                    <span class="menuText showHideSubMenu">Users</span>
+                    <i class="fa fa-angle-left mainMenuIconArrow showHideSubMenu"></i>
+                </a>
+                
+                <ul class="subMenus">
+                    <li><a class="subMenuLink" href="./user_view.php"><i class="fa fa-circle-o"></i>View Users</a></li>
+                    <li><a class="subMenuLink" href="./user_add.php"><i class="fa fa-circle-o"></i>Add Users</a></li>
+                </ul>
+            </li>
+<!--
+            <li class="liMainMenu">
+                <a href=""><i class="fa fa-file"></i><span class="menuText">Reports</span></a>
+            </li>
+           
+            <li class="liMainMenu">
                 <a href=""><i class="fa fa-line-chart"></i><span class="menuText">STATS</span></a>
-            </li>                    
+            </li>                  
+-->
         </ul>
     </div>
 </div>
