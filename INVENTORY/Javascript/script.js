bd5b46b025753250401e3436f0269c48293e605e
Hamna Iftikhar <96309521+hamnaiftikhar@users.noreply.github.com>
2023-08-25 14:22:43 +0500

    Update script.js
    updating js file

--- INVENTORY/Javascript/script.js
+++ INVENTORY/Javascript/script.js
@@ -37,3 +37,73 @@ toggle_btn.addEventListener( 'click', (event) => {
     }
 
 });
+
+// Submenu show / hide function
+document.addEventListener('click', function(e) {
+    let clickedEl = e.target;
+
+    if (clickedEl.classList.contains('showHideSubMenu')) {
+        e.preventDefault(); // Prevent default link behavior
+                
+        let subMenu = clickedEl.closest('li').querySelector('.subMenus');
+        let mainMenuIcon = clickedEl.closest('li').querySelector('.mainMenuIconArrow');
+        
+        
+        //close all submenus
+        
+        
+        let subMenus = document.querySelectorAll('.subMenus');
+        subMenus.forEach((sub) => {
+            if(subMenu !== sub) sub.style.display = 'none';
+        });
+        
+        
+        // call func to hide/show submenu
+        showHideSubMenu(subMenu, mainMenuIcon);
+
+
+        //console.log(targetMenu);
+    }
+});
+
+// function - to show/hide submenu
+
+function showHideSubMenu(subMenu, mainMenuIcon){
+            // check if there is a submenu
+    if (subMenu != null) {
+
+        if(subMenu.style.display === 'block'){
+            subMenu.style.display = 'none';
+            mainMenuIcon.classList.remove('fa-angle-down');
+            mainMenuIcon.classList.add('fa-angle-left');
+        } else{
+            subMenu.style.display = 'block';
+            mainMenuIcon.classList.remove('fa-angle-left');
+            mainMenuIcon.classList.add('fa-angle-down');
+        }
+
+    }
+}
+
+
+// add or hide active class to menu 
+// get the current page
+// use selector to get the current menu or submenu
+// add active class
+
+
+let pathArray = window.location.pathname.split('/');
+let curFile = pathArray[pathArray.length - 1];
+
+let curNav = document.querySelector('a[href="./'+ curFile +'"]');
+//curNav.classList.add('subMenuActive');
+
+let mainNav = curNav.closest('li.liMainMenu');
+mainNav.style.background = '#ED8DBA';
+
+let subMenu = curNav.closest('.subMenus');
+let mainMenuIcon = mainNav.querySelector('i.mainMenuIconArrow');
+
+
+// Call function to hide/show submenu
+showHideSubMenu(subMenu, mainMenuIcon);
